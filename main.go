90040d6e1ea1675a24cc4034c7c725dbbe604bf9
Dr Nic Williams <drnicwilliams@gmail.com>
2015-08-17 16:02:45 -0700

    dynamically display some data

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"github.com/cloudfoundry-community/gogobosh/models"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 )
 
 func dashboard(r render.Render) {
-	r.HTML(200, "dashboard", "test")
+	deployments := models.Deployments{
+		&models.Deployment{
+			Name: "legacy / sandbox / aws",
+			Releases: []models.NameVersion{
+				models.NameVersion{Name: "cf", Version: "211"},
+				models.NameVersion{Name: "cf-sensu-client", Version: "1"},
+			},
+			Stemcells: []models.NameVersion{
+				models.NameVersion{Name: "aws", Version: "3033"},
+			},
+		},
+		&models.Deployment{
+			Name: "legacy / dev / aws",
+			Releases: []models.NameVersion{
+				models.NameVersion{Name: "cf", Version: "211"},
+				models.NameVersion{Name: "cf-sensu-client", Version: "1"},
+			},
+			Stemcells: []models.NameVersion{
+				models.NameVersion{Name: "aws", Version: "3033"},
+			},
+		},
+		&models.Deployment{
+			Name: "legacy / prod / aws",
+			Releases: []models.NameVersion{
+				models.NameVersion{Name: "cf", Version: "205"},
+				models.NameVersion{Name: "cf-sensu-client", Version: "1"},
+			},
+			Stemcells: []models.NameVersion{
+				models.NameVersion{Name: "aws", Version: "3000"},
+			},
+		},
+	}
+	r.HTML(200, "dashboard", deployments)
 }
 
 func main() {
